73077203f58642d0a65351e1ad30f7cc05fb9021
Luciano Mendoza <lucho.201198@gmail.com>
2023-10-17 18:38:51 -0300

    Correccion de entrega

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 function calcularCuotas(monto, cuotas) {
     const montoPorCuota = monto / cuotas;
     return montoPorCuota;
@@ -8,12 +7,13 @@ const montoTotal = parseFloat(prompt("Ingrese el monto total:"));
 const numeroDeCuotas = parseInt(prompt("Ingrese el número de cuotas:"));
 
 
-if ( montoTotal => 0, numeroDeCuotas => 0) {
+if (isNaN(montoTotal)  || isNaN(numeroDeCuotas)) {
 
+    alert("Por favor, ingrese valores válidos.");
+   
+} else {
     const montoPorCuota = calcularCuotas(montoTotal, numeroDeCuotas);
 
     alert(`Monto por Cuota: ${montoPorCuota}`);
-   
-} else {
-    alert("Por favor, ingrese valores válidos.");
 }
+
